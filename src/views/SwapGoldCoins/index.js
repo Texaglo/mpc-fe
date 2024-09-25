94ee33572a324cb9f908d224422e0feb34bd2808
Rao <RaoMusharraf>
2024-09-25 15:46:40 +0500

    admin add token

--- src/views/SwapGoldCoins/index.js
+++ src/views/SwapGoldCoins/index.js
@@ -1,55 +1,115 @@
-import React, { useMemo, useState, useEffect } from 'react';
 import './index.css';
-import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { web3, BN } from '@project-serum/anchor';
+import { SwapAddress } from "../../store/contract/index";
+import { PublicKey, clusterApiUrl } from "@solana/web3.js";
+import React, { useMemo, useState, useEffect } from 'react';
+import { program, provider } from "../../store/solanaProvider";
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import { Program, web3, AnchorProvider, BN } from '@project-serum/anchor';
-import marketplaceABI from '../../store/contract/development/newSwap.json'; // Path to your IDL file
-import { PublicKey, Connection } from "@solana/web3.js";
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { getAssociatedTokenAddress, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
-const { SystemProgram } = web3;
-const programID = new PublicKey("CdEKZxHntobPE1VjfiqXgp2Av2rDmhGWV6JxGh5bQdx");
-const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+// const { SystemProgram } = web3;
+const programID = new PublicKey(SwapAddress);
+
 const Swap = () => {
 
-    // Create a network connection
     const network = clusterApiUrl('devnet');
     const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
-    const [goldToMpcValue, setGoldToMpcValue] = useState(0);
+    const [goldToMpcValue, setGoldToMpcValue] = useState("");
+    const [newGoldToMpcValue, setNewGoldToMpcValue] = useState("");
+    const [currentGoldToMpcValue, setCurrentGoldToMpcValue] = useState(0);
+    const [goldAmount, setGoldAmount] = useState(0);
+    const [mpcAmount, setMpcAmount] = useState(0);
+    const [mpc, setMpc] = useState("");
+
+
+    useEffect(() => {
+        getRate()
+    }, []);
 
     const global = async () => {
+        try {
+            const [vaultPda, _] = PublicKey.findProgramAddressSync(
+                [Buffer.from('Account20')],
+                programID
+            );
+            const tx = await program.rpc.globalAccount(new BN(goldToMpcValue), {
+                accounts: {
+                    vault: vaultPda,
+                    admin: provider.wallet.publicKey,
+                    systemProgram: web3.SystemProgram.programId
+                },
+            });
+            console.log("Transaction: ", tx);
+            setGoldToMpcValue("")
+            getRate()
+        } catch (error) {
+            console.log("******ERROR", error);
+        }
+    };
 
-        const provider = await getProvider()
-        const program = new Program(marketplaceABI, programID, provider);
-        const [vaultPda, _] = await PublicKey.findProgramAddressSync(
-            [Buffer.from('Account20')],
-            programID
-        );
-        const tx = await program.rpc.globalAccount(new BN(goldToMpcValue), {
-            accounts: {
-                vault: vaultPda,
-                admin: provider.wallet.publicKey,
-                systemProgram: SystemProgram.programId
-            },
-        });
-        console.log("********TX", tx);
-    }
+    const UpdateGoldValue = async () => {
+        try {
+            const [vaultPda, _] = PublicKey.findProgramAddressSync(
+                [Buffer.from('Account20')],
+                programID
+            );
+            const tx = await program.rpc.updateGoldValue(new BN(newGoldToMpcValue), {
+                accounts: {
+                    vault: vaultPda,
+                    admin: provider.wallet.publicKey
+                },
+            });
+            console.log("Transaction: ", tx);
+            setNewGoldToMpcValue("")
+            getRate()
+        } catch (error) {
+            console.log("******ERROR", error);
+        }
+    };
 
     const getRate = async () => {
-        const provider = await getProvider()
-        const program = new Program(marketplaceABI, programID, provider);
-        const [vaultPda, _] = await PublicKey.findProgramAddressSync(
+        const [vaultPda, _] = PublicKey.findProgramAddressSync(
             [Buffer.from('Account20')],
             programID
         );
         const data = await program.account.solAccount.fetch(vaultPda);
-        console.log("********data", data.goldToMpcValue.toString());
+        const currentRate = data.goldToMpcValue.toString();
+        setCurrentGoldToMpcValue(currentRate);
     };
 
-    const getProvider = () => {
-        const provider = new AnchorProvider(connection, window.solana, AnchorProvider.defaultOptions());
-        return provider;
+    const adminAddMpc = async () => {
+        try {
+            const MPC = new PublicKey("Fp3kdVYE7BiVjkQNtcWHEjhpL5ntpoBiBuRZtT8figTJ");
+            const [vaultPda] = PublicKey.findProgramAddressSync([Buffer.from('Account20')], programID);
+            const [globalAta] = PublicKey.findProgramAddressSync([Buffer.from("escrowTokenAccount20")], programID);
+            const mpcAdminAta = await getAssociatedTokenAddress(MPC, provider.wallet.publicKey);
+
+            const tx = await program.rpc.adminAddMpc(new BN(mpc), {
+                accounts: {
+                    escrowAccount: vaultPda,
+                    admin: provider.wallet.publicKey,
+                    adminTokenAccount: mpcAdminAta,
+                    escrowTokenAccount: globalAta,
+                    mint: MPC,
+                    tokenProgram: TOKEN_PROGRAM_ID,
+                    systemProgram: web3.SystemProgram.programId,
+                }
+            });
+            console.log("*******Transaction: ", tx);
+            setMpc("")
+        } catch (error) {
+            console.log("******ERROR", error);
+        }
+    };
+
+    const handleGoldSwap = (e) => {
+        const goldInput = e.target.value;
+        setGoldAmount(goldInput);
+        const mpc = goldInput / currentGoldToMpcValue;
+        setMpcAmount(mpc);
     };
 
     return (
@@ -57,6 +117,9 @@ const Swap = () => {
             <WalletProvider wallets={wallets} autoConnect>
                 <WalletModalProvider>
                     <div className="swap-container">
+                        <div className="current-rate">
+                            <h2>Current Rate: {currentGoldToMpcValue}</h2>
+                        </div>
                         <div className="swap-box">
                             <div className="rate-controls">
                                 <div className="input-group">
@@ -70,17 +133,62 @@ const Swap = () => {
                                     <button className="swap-button" onClick={global}>
                                         Set Rate
                                     </button>
-                                    <button className="swap-button" onClick={getRate}>
-                                        Current Rate
+
+                                    <input
+                                        type="number"
+                                        value={newGoldToMpcValue}
+                                        onChange={(e) => setNewGoldToMpcValue(e.target.value)}
+                                        placeholder="Enter new rate"
+                                    />
+                                    <button className="swap-button" onClick={UpdateGoldValue}>
+                                        Update Rate
                                     </button>
                                 </div>
+                            </div>
+                        </div>
+                        {/* New section for admin add mpc */}
+                        <div className="gold-to-mpc">
+                            <h3>Admin Add MPC Token</h3>
+                            <div className="input-group">
+                                <label>MPC Amount:</label>
+                                <input
+                                    type="number"
+                                    value={mpc}
+                                    onChange={(e) => setMpc(e.target.value)}
+                                    placeholder="Enter MPC Amount"
+                                />
+                                <div className="current-rate">
+                                    <button className="swap-button" onClick={adminAddMpc}>
+                                        Submit
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        {/* New section for swapping Gold to MPC */}
+                        <div className="gold-to-mpc">
+                            <h3>Swap Gold to MPC</h3>
+                            <div className="input-group">
+                                <label>Gold Amount:</label>
+                                <input
+                                    type="number"
+                                    value={goldAmount}
+                                    onChange={handleGoldSwap}
+                                    placeholder="Enter Gold amount"
+                                />
+                                <div className="current-rate">
+                                    <button className="swap-button" onClick={global}>
+                                        Swap
+                                    </button>
+                                </div>
+                                {/* <div className="current-rate">
+                                    <h3>MPC You Will Receive: {mpcAmount.toFixed(2)}</h3>
+                                </div> */}
                             </div>
                         </div>
                     </div>
                 </WalletModalProvider>
             </WalletProvider>
         </ConnectionProvider>
-
     );
 }
 
