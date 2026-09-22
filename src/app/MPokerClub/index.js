a7cdee9c38e4ec2ff28e3c10ecf3f0afbfdea61a
Texaglo-Technoking <90069572+Texaglo@users.noreply.github.com>
2026-09-22 10:22:05 -0500

    Update link in MPokerClub index.js

--- src/app/MPokerClub/index.js
+++ src/app/MPokerClub/index.js
@@ -158,7 +158,7 @@ class MPokerClub extends Component {
                                     <p className="hero-copy">Deposit crypto, buy MPCE time, join a table, and withdraw your balance when you are done.</p>
 
                                     <div className="btn-group hero-actions">
-                                        <a className='banner-btn' target="_blank" rel="noopener noreferrer" href='https://tony-update.d3fd413nat6wcy.amplifyapp.com/'>
+                                        <a className='banner-btn' target="_blank" rel="noopener noreferrer" href='https://main.du6evc0snv7lm.amplifyapp.com/'>
                                             <i className="play-icon"><img src={require('../../static/images/new-landing/play-icon.png')} alt='' /></i>
                                             <span>Start Playing</span>
                                             <i className="poker-icon"><img src={require('../../static/images/new-landing/poker-icon.png')} alt='' /></i>
